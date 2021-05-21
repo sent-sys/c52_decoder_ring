eedc32d65862d32f7b3530a379f17d7d911efebb
Matthew Ho <mattewho37@gmail.com>
2021-05-20 20:24:39 -0400

    Starting polybius

--- src/polybius.js
+++ src/polybius.js
@@ -7,7 +7,20 @@ const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
   function polybius(input, encode = true) {
-    // your solution code here
+    if(encode){
+      const key = {a:11, b:21, c:31, d:41, e:51, f:12, g:22, h:32, i: 42, j:42, k:52, l:13, m:23, 
+        n:33, o:43, p:53, q:14, r:24, s:34, t:44, u:54, v:15, w:25, x:35, y:45, z:55}
+      let output = ''
+      for(let letter of input){
+        if(letter == ' ') output += ' '
+        else output += key[letter]
+      }
+      return output
+    }
+    else{
+      const key = {42: '(i/j)'}
+
+    }
   }
 
   return {
--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,3 +1,16 @@
 // Write your tests here!
-const polybiusModule = require("../src/polybius")
+const {polybius} = require("../src/polybius")
 const expect = require("chai").expect
+
+describe("polybius()", () => {
+    it("should return correctly encrypted string when encoding = true", () => {
+        const actual = polybius('aaaa', true)
+        const expected = '11111111'
+        expect(actual).to.equal(expected)
+    })
+    if("should return correctly decrypted string when encoding = false", () => {
+        const actual = polybius('1111111')
+        const expected = 'aaaa'
+        expect(actual).to.equal(expected)
+    })
+})
